f3407174c1e4391766d4542de9605de51b15598d
Ebby Peter <ebby.peter@booster.co.nz>
2021-11-10 16:10:08 +1300

    adding more files

--- windows/WindowsPowerShell_profile.ps1
+++ windows/WindowsPowerShell_profile.ps1
@@ -1,7 +1,7 @@
 # Powerline
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Paradox
+#Import-Module posh-git
+#Import-Module oh-my-posh
+#Set-Theme Paradox
 
 # Proxy settings 
 (New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials

1e0c4fa4aee45cb21aa32206d88842725eb87b89
Ebby Peter <ebbypeter@outlook.com>
2025-06-09 16:20:26 +1200

    Changed oh-my-posh theme

--- appConfigs/powerShell/PowerShell_profile.ps1
+++ appConfigs/powerShell/PowerShell_profile.ps1
@@ -1,7 +1,17 @@
+Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
+
 # Powerline
 #Import-Module posh-git
 #Import-Module oh-my-posh
 #Set-Theme Paradox
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\pixelrobots.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config D:\personal\gh-ebbypeter\dotfiles\appConfigs\oh-my-posh\ebby-01.omp.json | Invoke-Expression
 
 # Proxy settings 
-(New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+#(New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+
+# PSReadline Intellisense
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
